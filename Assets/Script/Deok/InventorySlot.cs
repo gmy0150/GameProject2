0031267b20bfd43343a9204e51a38a4e6d28ffac
JD <dsu20211567@gmail.com>
2025-04-09 19:05:03 +0900

    0409 Item

---
+++ Assets/Script/Deok/InventorySlot.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class InventorySlot : MonoBehaviour
+{
+    public Image icon;
+    public RectTransform slotTransform;
+
+    private Tween currentTween;
+    private InterItem storedItem;
+
+    public void SetItem(Sprite itemIcon, InterItem item)
+    {
+        icon.sprite = itemIcon;
+        icon.enabled = true;
+        storedItem = item;
+    }
+
+    public void ClearItem()
+    {
+        icon.sprite = null;
+        icon.enabled = false;
+        storedItem = null;
+        SetSelected(false);
+    }
+
+    public bool HasItem()
+    {
+        return storedItem != null;
+    }
+
+    public void UseItem()
+    {
+        if (storedItem != null)
+        {
+            storedItem.UseItem();
+            ClearItem();
+        }
+    }
+
+    public void SetSelected(bool selected)
+    {
+        if (slotTransform == null)
+            slotTransform = GetComponent<RectTransform>();
+
+        currentTween?.Kill();
+
+        float targetScale = selected ? 1.15f : 1f;
+        currentTween = slotTransform.DOScale(targetScale, 0.2f).SetEase(Ease.OutQuad);
+    }
+}
