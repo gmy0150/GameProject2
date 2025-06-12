ac22e201892917a4f9a1bcfb280ac7331a2af71c
gmy0150 <gmy0150@naver.com>
2025-06-12 22:32:28 +0900

    trigger move

--- Assets/Script/M1n/InteractObj/SceneMoveBtn.cs
+++ Assets/Script/M1n/InteractObj/SceneMoveBtn.cs
@@ -3,45 +3,35 @@ using UnityEngine.UI;
 using DG.Tweening;
 using System.Collections;
 
-public class SceneMoveBtn : UseageInteract
+public class SceneMoveBtn : MonoBehaviour
 {
     public Image panelImage;
-    public float delayDuration = 3f;  
-    public float fadeDuration = 1.5f;     
+    public float delayDuration = 3f;
+    public float fadeDuration = 1.5f;
 
-    public override void Interact(Player character, IController controller)
+    void OnTriggerEnter(Collider other)
     {
-        base.Interact(character, controller);
-        Debug.Log("눌림");
-
-        GameObject prefab = Resources.Load<GameObject>("Prefabs/Second_Scene");
-        if (prefab != null)
+        if(other.CompareTag("Player"))
         {
-            Instantiate(prefab);
-        }
-
-        if (panelImage != null)
-        {
-            panelImage.gameObject.SetActive(true);
-            panelImage.color = new Color(0f, 0f, 0f, 1f);
-
-            DOVirtual.DelayedCall(delayDuration, () =>
+            GameObject prefab = Resources.Load<GameObject>("Prefabs/Second_Scene");
+            if (prefab != null)
             {
-                panelImage.DOFade(0f, fadeDuration).OnComplete(() =>
+                Instantiate(prefab);
+            }
+
+            if (panelImage != null)
+            {
+                panelImage.gameObject.SetActive(true);
+                panelImage.color = new Color(0f, 0f, 0f, 1f);
+
+                DOVirtual.DelayedCall(delayDuration, () =>
                 {
-                    panelImage.gameObject.SetActive(false);
+                    panelImage.DOFade(0f, fadeDuration).OnComplete(() =>
+                    {
+                        panelImage.gameObject.SetActive(false);
+                    });
                 });
-            });
+            }
         }
-    }
-
-    public override bool IsOneTimeInteraction()
-    {
-        return true;
-    }
-
-    public override void UpdateTime(float time)
-    {
-        throw new System.NotImplementedException();
     }
 }
